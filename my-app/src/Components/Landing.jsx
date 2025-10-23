ca858ce57be83513c6556b41580f401aa8a23d13
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-23 21:15:59 +0100

    for the manipulation of landing page for the user interface

--- my-app/src/Components/Landing.jsx
+++ my-app/src/Components/Landing.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { motion } from 'framer-motion'
-import { FaDownload, FaWhatsapp } from 'react-icons/fa'
-import profileImg from '../assets/Image/profile.jpg'
+import {motion} from 'framer-motion'
+import {FaDownload, FaWhatsapp} from 'react-icons/fa'
+import profileImg from '../assets/Images/profile.jpg'
 
 function Landing() {
   const handleDownloadCV = () => {
@@ -15,12 +15,11 @@ function Landing() {
     }
   }
 
-  // const handleHireMe = () => {
-  //   const subject = 'Hiring Inquiry - Project Discussion'
-  //   const body = 'Hello Emmanuel Adah,\n\nI saw your portfolio and would like to discuss a potential project opportunity with you.\n\nPlease let me know your availability for a call.\n\nBest regards'
-  //   const mailtoUrl = `mailto:edogbanya02@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`
-  //   window.location.href = mailtoUrl
-  // }
+  const handleHireMe = () => {
+    const subject = 'Hiring Inquiry - Project Discussion'
+    const body = 'Hello Emmanuel Adah,\n\nI saw your portfolio and would like to discuss a potential project opportunity with you.\n\nPlease let me know your availability for a call.\n\nBest regards'
+    window.location.href = `mailto:edogbanya02@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`
+  }
 
   const handleWhatsApp = () => {
     const phoneNumber = '+2348160509785'
@@ -67,29 +66,22 @@ function Landing() {
                 <FaWhatsapp /> WhatsApp Me
               </motion.button>
               
-              {/*<motion.button */}
-              {/*  className="btn btn-outline"*/}
-              {/*  onClick={handleHireMe}*/}
-              {/*  whileHover={{ scale: 1.05 }}*/}
-              {/*  whileTap={{ scale: 0.95 }}*/}
-              {/*>*/}
-              {/*  Hire Me*/}
-              {/*</motion.button>*/}
+              <motion.button
+                className="btn btn-outline"
+                onClick={handleHireMe}
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                Hire Me
+              </motion.button>
             </div>
           </motion.div>
-          
-          <motion.div 
-            className="landing-image"
-            initial={{ opacity: 0, x: 50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.8, delay: 0.2 }}          
-          >
-            <div className="profile-image">
-              <div className="placeholder-image">
-                <img src={profileImg} alt="Profile" />
-                <span>Your Photo</span>
-              </div>
-            </div>
+
+          <motion.div className="placeholder-image"
+               initial={{ opacity: 0, x: 50 }}
+               animate={{ opacity: 1, x: 0 }}
+               transition={{ duration: 0.8, delay: 0.2 }} >
+            <img src={profileImg} alt="Profile" />
           </motion.div>
         </div>
       </div>
